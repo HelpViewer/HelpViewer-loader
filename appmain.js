db5ebf5e04c008b4c3d9bed29c166d3eeaf955da
VSimon <vita.simon@centrum.cz>
2026-03-13 14:15:51 +0100

    Loader: filtering of UTF characters (invisible-code) from texts where attack can be possible (unicode steganography) - safeFilteredText - added skipping SMP variation selectors range

--- appmain.js
+++ appmain.js
@@ -14,15 +14,24 @@ function toFilteredUTFText(s) {
   // \u0000-\u001F (C0), \u007F-\u009F (C1)
   // \u2028 - line sep, \u2029 - par sep.
   // \u0000-\u001F ... kept unsolved because of browser loading stooped due to this!
+  // \uFE00–\uFE0F - think about this - fall unicode icons to no color forms, but improves security
 }
 
 function safeFilteredText(s) {
   if (!s) return s;
-  return s.split('').filter((c, i) => {
-      const code = c.charCodeAt(0);
-      return code >= 32 || [9,10,13].includes(code);
-    })
-    .join('');
+  let out = '';
+  for (const ch of s) {
+    const cp = ch.codePointAt(0);
+
+    // skip: SMP variation selectors
+    if (cp >= 0xE0100 && cp <= 0xE01EF) continue;
+
+    // keep: TAB (9), LF (10), CR (13), 32 and higher
+    if (cp >= 32 || cp === 9 || cp === 10 || cp === 13) {
+      out += ch;
+    }
+  }
+  return out;
 }
 
 function newUID(length = 8) {
